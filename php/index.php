b3f1b6b8cf306b38e766a97fb5b3f9f708edf00e
geovannyha <geovannyha@gmail.com>
2023-06-07 20:24:08 -0600

    este es un commit de un parrafo de texto agregado

--- php/index.php
+++ php/index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>hola mundo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, officia cupiditate aliquid ex quo expedita laboriosam dolore nesciunt cumque, veritatis, dolores delectus ad repellat explicabo et maxime. Fuga, impedit iure?</p>
     
 </body>
